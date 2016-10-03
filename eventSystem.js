1d1f05dd103af476de98770c765970709c825d62
jan <jan@ruken.pw>
2016-10-03 11:45:44 +0200

    initial commit

---
+++ eventSystem.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class EventSystem {
+  constructor() {
+    this._events = new Map();
+    this._remoteEvents = new Map();
+  }
+
+  _genericAdd(map, name, func) {
+    if (map.has(name)) {
+      map.get(name).push(func);
+    } else {
+      map.set(name, [func]);
+    }
+  }
+
+  Add(name, func) {
+    this._genericAdd(this._events, name, func);
+  }
+
+  AddRemoteCallable(name, func) {
+    this._genericAdd(this._remoteEvents, name, func);
+  }
+}
+
+module.exports = EventSystem;
---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+global.log = require('custom-logger').new({
+    debug: { color: 'grey', level: 0, event: 'debug' },
+    stub: { color: 'cyan', level: 1, event: 'stub' },
+    info: { color: 'green', level: 2, event: 'info' },
+    warn: { color: 'yellow', level: 3, event: 'warning' },
+    error: { color: 'red', level: 4, event: 'ERROR' }
+});
+
+const { EventSystem } = require('./eventSystem');
+const _serverData = require('./data_server.json');
+const { ClassBuilder } = require('./classBuilder');
+
+module.exports = {
+    events: new EventSystem(),
+    jcmp: {
+
+    },
+};
