bd50c04e336f29d5b9d5759e08b1ee95f47af562
jan <jan@ruken.pw>
2016-10-03 17:09:04 +0200

    more constructors

--- constructors.js
+++ constructors.js
@@ -1,11 +1,5 @@
 'use strict';
 
-function vectorConstructor(v, ...args) {
-  v.x = args[0];
-  v.y = args[1],
-  v.z = args[2];
-}
-
 const constructors = {
   Vector3f: function(x = 0, y = 0, z = 0) { this.x = x; this.y = y; this.z = z; },
   Vector3: function(x = 0, y = 0, z = 0) { this.x = x; this.y = y; this.z = z; },
@@ -13,6 +7,13 @@ const constructors = {
   Vector2: function(x = 0, y = 0) { this.x = x; this.y = y; },
   Vector4f: function(x = 0, y = 0, z = 0, w = 0) { this.x = x; this.y = y; this.z = z; this.w = w; },
   Vector4: function(x = 0, y = 0, z = 0, w = 0) { this.x = x; this.y = y; this.z = z; this.w = w; },
-}
+  RGB: function(r = 0, g = 0, b = 0) { this.r = r; this.g = g; this.b = b; },
+  RGBA: function(r = 0, g = 0, b = 0, a = 0) { this.r = r; this.g = g; this.b = b; this.a = a; },
+};
+
+constructors.Vehicle = function(hash, pos = new Vector3(), rot = new Vector3()) { this.modelHash = hash; this.position = pos; this.rotation = rot };
+constructors.Checkpoint = function(hash, pos = new Vector3(), rot = new Vector3()) { this.modelHash = hash; this.position = pos; this.rotation = rot };
+constructors.GameObject = function(hash, pos = new Vector3(), rot = new Vector3()) { this.model = hash; this.position = pos; this.rotation = rot };
+constructors.POI = function(type, pos = new Vector3(), rot = new Vector3()) { this.type = type; this.position = pos; this.rotation = rot };
 
 module.exports = constructors;
